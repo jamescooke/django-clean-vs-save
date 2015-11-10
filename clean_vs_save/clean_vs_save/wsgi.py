eeb22c68661f4534ba7d661e40ab96186e93e62f
James Cooke <jamescooke@users.noreply.github.com>
2015-11-10 14:14:01 +0000

    Adjust app name to clean_vs_save

--- clean_vs_save/clean_vs_save/wsgi.py
+++ clean_vs_save/clean_vs_save/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for talky project.
+WSGI config for clean_vs_save project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "talky.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clean_vs_save.settings")
 
 application = get_wsgi_application()
